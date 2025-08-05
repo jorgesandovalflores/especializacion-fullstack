40acee013c06bc3eddf6765d63cffe9c538ec017
jorgesandovalflores <jorge.ramos.sandoval.flores@gmail.com>
2025-08-05 00:16:26 -0500

    clase 04/modulo03

--- backend-nodejs/modulo-03/session-04/example/jest.config.js
+++ backend-nodejs/modulo-03/session-04/example/jest.config.js
@@ -2,20 +2,21 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'js', 'json'],
-    rootDir: 'src',
-    testRegex: '.*\.spec\.ts$',
     transform: {
         '^.+\\.(t|j)s$': 'ts-jest',
     },
     collectCoverage: true,
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: ['src/**/*.{ts,js}'],
+    coverageReporters: ['text', 'lcov', 'html'],
     reporters: [
         'default',
         [
-            'jest-html-reporter',
-            {
+        'jest-html-reporter',
+        {
             pageTitle: 'Test Report',
             outputPath: 'coverage/report.html',
-            },
+        },
         ],
     ],
 };
