bd57930f0949e9c6de3c77da3b09432be3cca30e
jorgesandovalflores <jorge.ramos.sandoval.flores@gmail.com>
2026-02-26 20:01:56 -0500

    clase 04 / módulo 04 - curso frontend

--- frontend-vue/modulo-04/session-03/example-frontend/src/components/ProductList.vue
+++ frontend-vue/modulo-04/session-03/example-frontend/src/components/ProductList.vue
@@ -1,25 +1,39 @@
 <!-- src/components/ProductList.vue -->
 <template>
-	<div class="row">
-		<div class="col-md-4 mb-3" v-for="product in products" :key="product.id">
-			<div class="card">
-				<img :src="product.image" class="card-img-top" />
-				<div class="card-body">
-					<h5 class="card-title">{{ product.name }}</h5>
-					<p class="card-text">S/ {{ product.price }}</p>
-					<button class="btn btn-primary w-100" @click="cart.addToCart(product)">
-						Agregar al carrito
-					</button>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="row">
+        <div
+            class="col-md-4 mb-3"
+            v-for="product in products"
+            :key="product.id"
+        >
+            <div class="card">
+                <img :src="product.image" class="card-img-top" />
+                <div class="card-body">
+                    <h5 class="card-title">{{ product.name }}</h5>
+                    <p class="card-text">S/ {{ product.price }}</p>
+                    <button
+                        class="btn btn-primary w-100"
+                        @click="cart.addToCart(product)"
+                    >
+                        Agregar al carrito
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { useProductStore } from '../stores/product.store'
-import { useCartStore } from '../stores/cart.store'
+import { onMounted } from "vue";
+import { useProductStore } from "../stores/product.store";
+import { useCartStore } from "../stores/cart.store";
 
-const { products } = useProductStore()
-const cart = useCartStore()
+const { products } = useProductStore();
+const cart = useCartStore();
+
+onMounted(() => {
+    setTimeout(() => {
+        throw new Error("Error lanzado después de 5 segundos en onMounted");
+    }, 5000);
+});
 </script>
--- frontend-vue/modulo-04/session-03/example-frontend/src/main.ts
+++ frontend-vue/modulo-04/session-03/example-frontend/src/main.ts
@@ -1,20 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 import * as Sentry from "@sentry/vue";
-import LogRocket from 'logrocket';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import LogRocket from "logrocket";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const app = createApp(App)
+const app = createApp(App);
 Sentry.init({
-  app,
-  dsn: "https://fb38148e028841da8d12c0d9e02a59b2@o4509369188679680.ingest.us.sentry.io/4509369190055936",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true
+    app,
+    dsn: "https://cd938d7c612a4eca2807e9a83a9a5c9c@o4510955156340736.ingest.us.sentry.io/4510955165122560",
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true,
 });
-LogRocket.init('frbxh8/javascript-vue');
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+LogRocket.init("frbxh8/javascript-vue");
+app.use(createPinia());
+app.use(router);
+app.mount("#app");
